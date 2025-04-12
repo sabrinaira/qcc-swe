d4d99bae7d608a17213d28a639bac0e12024d485
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-04-11 21:21:38 -0400

    day 11 notes

---
+++ 04-11-2025_DAY11/april11.js
@@ -0,0 +1,29 @@
+document.querySelector('button').addEventListener('click', () => {
+  document.querySelector('h1').textContent = 'COKE + SPRITE';
+  document.querySelector('h1').style.color = 'orange';
+  document.querySelector('h1').style.fontSize = '50px';
+});
+
+// alert('Hello again!');
+
+console.log('Hello John Doe');
+
+const a = 'coffee';
+console.log(a);
+
+const prod1 = 'Kona Coffee';
+
+console.log(prod1);
+
+const prod2 = 'Lemon Tea';
+
+console.log(prod2);
+
+let prod3 = 'ICE CREAM';
+
+prod3 = 'cookie';
+
+console.log(prod3);
+
+let sum = 2 + 2;
+console.log(sum);
