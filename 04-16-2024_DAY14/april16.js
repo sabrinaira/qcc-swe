33d2f62cabe93010795ebfae2cdaa280259d45ea
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-04-19 09:14:30 -0400

    Day 14 class code

--- 04-16-2024_DAY14/april16.js
+++ 04-16-2024_DAY14/april16.js
@@ -7,7 +7,32 @@ $('h1').text('Updated jQuery Title');
 $('h1').css('color','red')
 $('h1').css('border','4px solid green')
 
-$('button').on('click', function()){
+$('button').on('click', function()) {
   $('p').fadeToggle;
   $('p').text('Happy Wednesday!');
-}
+};
+
+// ----------------------------------
+
+$('button').text('check more info');
+
+$('h1').text('Updated jQuery Title');
+
+$('h1').css('color','red');
+$('h1').css('border','4px solid green');
+
+
+$('h1').html('<i>Italic Title</i>');
+
+
+// $('button').click(function(){
+//     $('p').fadeToggle();
+//     $('p').text('Happy Wednesday!')
+// })
+
+
+
+$('button').on('click',function(){
+    $('p').fadeToggle();
+    $('p').text('Happy Wednesday!')
+})
